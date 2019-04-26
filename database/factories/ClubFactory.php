7a0dbb4e3a9202758b0f075be6e274e8948ca1a4
amirmasoud <amirmasood33@gmail.com>
2019-04-27 04:27:15 +0430

    Shake unused dbs

--- database/factories/ClubFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\Club::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
--- database/factories/ScoreFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\Score::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
