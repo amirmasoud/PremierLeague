330bd4c9afc5c1c2e0b480c8cb34a2f0ba8cdb46
amirmasoud <amirmasood33@gmail.com>
2019-04-26 18:23:51 +0430

    Remove unnecessery auth methods

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -4,47 +4,14 @@
       <router-link :to="{ name: user ? 'home' : 'welcome' }" class="navbar-brand">
         {{ appName }}
       </router-link>
-
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false">
-        <span class="navbar-toggler-icon"/>
-      </button>
     </div>
   </nav>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import LocaleDropdown from './LocaleDropdown'
-
 export default {
-  components: {
-    LocaleDropdown
-  },
-
   data: () => ({
     appName: window.config.appName
-  }),
-
-  computed: mapGetters({
-    user: 'auth/user'
-  }),
-
-  methods: {
-    async logout () {
-      // Log out the user.
-      await this.$store.dispatch('auth/logout')
-
-      // Redirect to login.
-      this.$router.push({ name: 'login' })
-    }
-  }
+  })
 }
 </script>
-
-<style scoped>
-.profile-photo {
-  width: 2rem;
-  height: 2rem;
-  margin: -.375rem 0;
-}
-</style>
